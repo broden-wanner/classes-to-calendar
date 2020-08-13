52cb97ffe3651d93e291f221098f3c99dcc00ffe
Broden Wanner <broden.wanner@outlook.com>
2020-08-13 01:11:41 -0500

    Changed footer to an appbar

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,34 +1,57 @@
-import React from 'react';
-import { Fab, makeStyles } from '@material-ui/core';
-import DataUsageIcon from '@material-ui/icons/DataUsage';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-const useStyle = makeStyles(theme => ({
+const useStyle = makeStyles((theme) => ({
   footer: {
-    position: 'absolute',
-    top: 'auto',
-    bottom: '0',
-    padding: '24px',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'flex-end'
+    position: "absolute",
+    top: "auto",
+    bottom: "0",
+    width: "100%",
   },
   extendedIcon: {
-    marginRight: '8px'
-  }
+    marginRight: "8px",
+  },
+  footerLink: {
+    color: theme.palette.grey[400],
+    opacity: 0.5,
+  },
+  divder: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    color: theme.palette.grey[400],
+    opacity: 0.5,
+  },
 }));
 
 function Footer() {
   const classes = useStyle();
-  const history = useHistory();
 
   return (
-    <div className={classes.footer}>
-      <Fab variant="extended" color="primary" onClick={() => history.push('/privacy-policy')}>
-        <DataUsageIcon className={classes.extendedIcon} />
-        Privacy Policy
-      </Fab>
-    </div>
+    <AppBar position="fixed" color="primary" className={classes.footer}>
+      <Toolbar>
+        <Grid container justify="center" alignItems="center">
+          <Link to="/privacy-policy" className={classes.footerLink}>
+            Privacy Policy
+          </Link>
+          <span className={classes.divder}>|</span>
+          <Link to="/contact" className={classes.footerLink}>
+            Contact
+          </Link>
+          <span className={classes.divder}>|</span>
+
+          <Typography component="p" variant="p" className={classes.footerLink}>
+            &#169; 2020 Broden Wanner
+          </Typography>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 }
 
