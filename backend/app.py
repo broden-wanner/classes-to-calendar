ea942e4ca67fa6f0a8ac049fdbb1ebaff685cac9
Broden Wanner <broden.wanner@outlook.com>
2020-08-15 01:21:19 -0500

    Added dotenv calls to app and wsgi

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,10 @@
 from classestocalendar import app
 from classestocalendar import settings
+from dotenv import load_dotenv, find_dotenv
+
+# Load environment variables from the .env file
+load_dotenv(find_dotenv())
+
 
 if __name__ == '__main__':
     if settings.APP_HOST:
